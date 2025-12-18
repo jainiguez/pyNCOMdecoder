c123a966fc37972e93a4133889a0538317f81a9b
Jon Ander Iniguez de Gordoa <jainiguez@vicomtech.org>
2025-12-18 16:52:57 +0100

    update ncom msg fields

--- pyNCOM/NCOMmsg.py
+++ pyNCOM/NCOMmsg.py
@@ -1,5 +1,8 @@
 from collections import OrderedDict
 from bitstring import BitArray
+import math
+import struct
+import time
 
 ACC_FACTOR = 1e-4
 ANG_FACTOR = 1e-5
@@ -14,9 +17,11 @@ class NCOM(object):
         self._names = ('Sync', 'Time',
                       'AccX', 'AccY', 'AccZ',
                       'AngX', 'AngY', 'AngZ',
-                      'NavStat', 'Lat', 'Long', 'Alti',
+                      'NavStat', 'Checksum_1', 'Lat', 'Long', 'Alti',
                       'Vel_North', 'Vel_East', 'Vel_Down',
-                      'Heading', 'Pitch', 'Roll')
+                      'Heading', 'Pitch', 'Roll',
+                      'Checksum_2', 'Status_channel',
+                      'Status')
         self._navstat = {
             0: '0: All quantities in the packet are invalid',
             1: '1: Raw IMU measurements',
@@ -39,9 +44,12 @@ class NCOM(object):
         self._data = b.unpack(('uintle:8, uintle:16,'   # Sync, Time
                                'intle:24, intle:24, intle:24,'  # AccX, AccY, AccZ
                                'intle:24, intle:24, intle:24,'  # AngX, AngY, AngZ
-                               'uintle:8, uintle:64, uintle:64, uintle:32,'  # NavStat, Lat, Long, Alti
-                               'uintle:24, uintle:24, uintle:24,'  # Vel North, Vel East, Vel Down
-                               'uintle:24, uintle:24, uintle:24'  # Heading, Pitch, Roll
+                               'uintle:8, uintle:8, ' # NavStat, Checksum_1
+                               'floatle:64, floatle:64, uintle:32,'  # NavStat, Lat, Long, Alti
+                               'intle:24, intle:24, intle:24,'  # Vel North, Vel East, Vel Down
+                               'intle:24, uintle:24, uintle:24, '  # Heading, Pitch, Roll
+                               'uintle:8, uintle:8, '  # Checksum_2, Status_channel
+                               'bytes:8'
                                ))
         self.d = OrderedDict(zip(self._names, self._data))
         assert self.d['Sync'] == 231, print("Sync byte not matched:0x{:x}".format(self.d['Sync'])) # Always 0xE7
@@ -54,7 +62,9 @@ class NCOM(object):
         self.d['Vel_North'] *= VEL_FACTOR
         self.d['Vel_East'] *= VEL_FACTOR
         self.d['Vel_Down'] *= VEL_FACTOR
-        self.d['Heading'] *= IMU_FACTOR
-        self.d['Pitch'] *= IMU_FACTOR
-        self.d['Roll'] *= IMU_FACTOR
+        self.d['Heading'] *= IMU_FACTOR * 180 / math.pi
+        self.d['Pitch'] *= IMU_FACTOR * 180 / math.pi
+        self.d['Roll'] *= IMU_FACTOR * 180 / math.pi
+        self.d['Lat'] *= 180 / math.pi
+        self.d['Long'] *= 180 / math.pi
         self.d['NavStat'] = self._navstat.get(self.d['NavStat'], 'Reserved')
